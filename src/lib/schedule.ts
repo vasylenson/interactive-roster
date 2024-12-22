645d36b41a52caf8e9fc89db828082b953d6cfe0
Marko Vasylenko <marko@elnino.tech>
2024-12-22 18:49:25 +0100

    separate iteration logic form the component

--- src/lib/schedule.ts
+++ src/lib/schedule.ts
@@ -1,4 +1,99 @@
-import { permute, subsetsOf } from "./random";
+import { addWeek, isStartOfMonth } from './date';
+import { permute, seed, subsetsOf } from './random';
+
+export class Schedule {
+    private weeks: Assignment[];
+    private people: Person[];
+    private tasks: Task[];
+    readonly startDate: Date;
+
+    constructor(startDate: Date, people: Person[], tasks: Task[]) {
+        this.startDate = startDate;
+        this.people = people;
+        this.tasks = tasks;
+        this.weeks = [];
+    }
+
+    pushWeek(assignment: Assignment) {
+        this.weeks.push(assignment);
+    }
+
+    lock(assignments: Assignment[]) {
+        for (const assignment of assignments) this.pushWeek(assignment);
+    }
+
+    get endDate() {
+        const endDate = new Date();
+        endDate.setDate(this.startDate.getDate() + 7 * this.weeks.length);
+        return endDate;
+    }
+
+    /**
+     * Iterate through the weeks of the schedule, starting at the start date,
+     * and generating new weeks on demand indefinitely.
+     */
+    assignments() {
+        const generator = function* (lockedSchedule: Assignment[], people: Person[], tasks: Task[]) {
+            let counters = initCounters(people, names(tasks));
+            let date = new Date('09-02-2024');
+            console.log({ date });
+            seed(912312423333);
+
+            for (const week of lockedSchedule) {
+                yield [date, tasks.map(({ name }) => week[name] ?? [])] as [
+                    Date,
+                    string[][]
+                ];
+                updateCounters(counters, week);
+                addWeek(date);
+                console.log({ date });
+            }
+
+            for (let weekNum = lockedSchedule.length; true; weekNum++)
+                try {
+                    if (date.toString() === new Date('12-02-2024').toString()) {
+                        const ivo = 'Ivo' as Person;
+                        people = people.filter((person) => person != 'Marlou');
+                        people.push(ivo);
+                        addPerson(ivo, counters);
+                    }
+
+                    let pool =
+                        weekNum > 5 && weekNum < 10
+                            ? people.filter((person) => person != 'Alex')
+                            : people;
+                    pool =
+                        weekNum == 6
+                            ? exclude(pool, ['Estephania'] as Person[])
+                            : pool;
+
+                    const assignment = nextWeekTasks(
+                        pool,
+                        isStartOfMonth(date) ? tasks : weekly(tasks),
+                        counters
+                    );
+                    yield [
+                        new Date(date),
+                        tasks.map(({ name }) => assignment[name] ?? []),
+                    ] as [Date, string[][]];
+                    addWeek(date);
+                } catch (e) {
+                    console.log(e);
+                    return;
+                }
+        };
+
+        return generator(this.weeks, this.people, this.tasks);
+    }
+}
+
+export const names = (tasks: Task[]) => tasks.map(({ name }) => name);
+
+const weekly = (tasks: Task[]) =>
+    tasks.filter(({ kind }) => kind !== repeat.monthly);
+
+const exclude = (people: readonly Person[], excluded: Person[]) =>
+    people.filter((person) => !excluded.includes(person));
 
 export type Assignment = Record<TaskName, Person[]>;
 
@@ -31,7 +126,10 @@ export type TaskName = string & { _brand: typeof BrandTaskName };
 
 type Counters = Record<TaskName, Record<Person, Counter>>;
 
-export function initCounters(people: readonly Person[], tasks: readonly TaskName[]) {
+export function initCounters(
+    people: readonly Person[],
+    tasks: readonly TaskName[]
+) {
     const counters: Counters = {};
     for (const taskName of tasks) {
         counters[taskName] = {};
@@ -46,9 +144,9 @@ export function initCounters(people: readonly Person[], tasks: readonly TaskName
 }
 
 /**
- * 
+ *
  * @param people considered in the selection. Don't have to be all the people mentioned in counters.
- * @param tasks 
+ * @param tasks
  * @param counters are mutated
  * @returns the record of tasks to arrays of assigned people.
  */
@@ -57,13 +155,19 @@ export function nextWeekTasks(
     tasks: readonly Task[],
     counters: Counters,
     heuristic: Heuristic = score,
-    scoreCombo: ScoreCombo = scorePeopleCombination,
+    scoreCombo: ScoreCombo = scorePeopleCombination
 ) {
     function getCandidates(spread: number = 3) {
         const candidates: Record<TaskName, [Person, number][]> = {};
         for (const task of tasks) {
             candidates[task.name] = Array.from(permute(people))
-                .map((person) => [person, heuristic(person, task, counters)] as [Person, number])
+                .map(
+                    (person) =>
+                        [person, heuristic(person, task, counters)] as [
+                            Person,
+                            number
+                        ]
+                )
                 .sort(([_1, score1], [_2, score2]) => score1 - score2)
                 .slice(0, task.people + spread);
         }
@@ -85,24 +189,39 @@ export function nextWeekTasks(
             }
 
             const [task, ...remainingTasks] = tasks;
-            for (const peopleAndScores of subsetsOf(task.people, candidates[task.name])) {
+            for (const peopleAndScores of subsetsOf(
+                task.people,
+                candidates[task.name]
+            )) {
                 const assignees = peopleAndScores.map(left);
-                const score = peopleAndScores.map(right).reduce(add) + scoreCombo(assignees);
+                const score =
+                    peopleAndScores.map(right).reduce(add) +
+                    scoreCombo(assignees);
 
                 const assigneesSet = new Set(assignees);
-                if (!people.isSupersetOf(assigneesSet)) continue; // if some people are already assigned 
+                if (!people.isSupersetOf(assigneesSet)) continue; // if some people are already assigned
 
-                const next = {...current, [task.name]: assignees};
+                const next = { ...current, [task.name]: assignees };
                 const remainingPeople = people.difference(assigneesSet);
 
-                yield* makeAssignments(next, currentScore + score, remainingTasks, remainingPeople)
+                yield* makeAssignments(
+                    next,
+                    currentScore + score,
+                    remainingTasks,
+                    remainingPeople
+                );
             }
         }
 
         let minScore = Infinity;
         let bestAssignment = null;
         let count = 0;
-        for (const [assignment, score] of makeAssignments({}, 0, tasks, new Set(people))) {
+        for (const [assignment, score] of makeAssignments(
+            {},
+            0,
+            tasks,
+            new Set(people)
+        )) {
             if (score < minScore) {
                 minScore = score;
                 bestAssignment = assignment;
@@ -111,7 +230,7 @@ export function nextWeekTasks(
         }
 
         // console.log({ count });
-        
+
         if (bestAssignment === null) continue;
 
         updateCounters(counters, bestAssignment);
@@ -119,7 +238,7 @@ export function nextWeekTasks(
         return bestAssignment;
     }
 
-    throw new Error("Could not choose anything");
+    throw new Error('Could not choose anything');
 }
 
 export function updateCounters(counters: Counters, assignment: Assignment) {
@@ -128,7 +247,9 @@ export function updateCounters(counters: Counters, assignment: Assignment) {
             const counter = counters[taskName as TaskName][person];
 
             if (!counter) {
-                console.error(`No counter for ${taskName} by ${person}, skipping`);
+                console.error(
+                    `No counter for ${taskName} by ${person}, skipping`
+                );
                 continue;
             }
             counter.timesDone++;
@@ -148,8 +269,8 @@ export function addPerson(person: Person, counters: Counters) {
     for (const [taskName, taskCounters] of Object.entries(counters)) {
         const individualCounters = Object.values(taskCounters);
         const numPeople = individualCounters.length;
-        
-        let timesDone = 0
+
+        let timesDone = 0;
         for (const counter of individualCounters) {
             timesDone += counter.timesDone;
         }
@@ -159,17 +280,15 @@ export function addPerson(person: Person, counters: Counters) {
         counters[taskName as TaskName][person] = {
             timesDone,
             weeksSinceDone: numPeople,
-        }
-    } 
+        };
+    }
 }
-
 
 /**
  * A function that assigns a score to a person and a task based on counters.
  * The higher the score the less likely the person will have to do the task.
  */
 type Heuristic = (person: Person, task: Task, counters: Counters) => number;
-
 
 const score: Heuristic = (person, task, counters) => {
     let { timesDone, weeksSinceDone } = counters[task.name][person];
@@ -178,9 +297,10 @@ const score: Heuristic = (person, task, counters) => {
         .reduce(min);
 
     const numPeople = Object.values(counters[task.name]).length;
-    const timeTaskDoneAverage = Object.values(counters[task.name])
-        .map(({ timesDone }) => timesDone)
-        .reduce(add) / numPeople;
+    const timeTaskDoneAverage =
+        Object.values(counters[task.name])
+            .map(({ timesDone }) => timesDone)
+            .reduce(add) / numPeople;
 
     timesDone -= timeTaskDoneAverage;
 
@@ -191,7 +311,12 @@ const score: Heuristic = (person, task, counters) => {
 
     const a = weeksSinceDone < 7 ? 5 : 2 / weeksSinceDone;
     const b = timesDone < 5 ? 3 : 1 / timesDone;
-    const c = weeksSinceDoneAnyTask < 2 ? 10 : weeksSinceDoneAnyTask < 3 ? 3 : 1 / weeksSinceDoneAnyTask;
+    const c =
+        weeksSinceDoneAnyTask < 2
+            ? 10
+            : weeksSinceDoneAnyTask < 3
+            ? 3
+            : 1 / weeksSinceDoneAnyTask;
 
     // console.log({ score: 1 + a * b + c });
 
@@ -201,18 +326,22 @@ const score: Heuristic = (person, task, counters) => {
 type ScoreCombo = (people: Person[]) => number;
 
 const scorePeopleCombination: ScoreCombo = (people) => {
-    const not = (one: string, other: string) => 
-        people.includes(one as Person) && people.includes(other as Person) ? 100000 : 0;
+    const not = (one: string, other: string) =>
+        people.includes(one as Person) && people.includes(other as Person)
+            ? 100000
+            : 0;
 
-    return not('Marko', 'Diego') 
-        + not('Marko', 'Marlou')
-        + not('Marko', 'Michelle')
-        + not('Marko', 'Estephania')
-        + not('Olga', 'Marlou')
-        + not('Olga', 'Michelle')
+    return (
+        not('Marko', 'Diego') +
+        not('Marko', 'Marlou') +
+        not('Marko', 'Michelle') +
+        not('Marko', 'Estephania') +
+        not('Olga', 'Marlou') +
+        not('Olga', 'Michelle')
+    );
 };
 
-const min = (a: number, b: number) => a < b ? a : b;
+const min = (a: number, b: number) => (a < b ? a : b);
 
 const add = (a: number, b: number) => a + b;
 
--- src/routes/(schedule).tsx
+++ src/routes/(schedule).tsx
@@ -1,164 +1,141 @@
 import { createSignal, For, Match, Switch } from "solid-js";
-import { addWeek, isStartOfMonth } from "~/lib/date";
-import { seed } from "~/lib/random";
-import { addPerson, Assignment, initCounters, nextWeekTasks, Person, repeat, Task, TaskName, updateCounters } from "~/lib/schedule";
+import {
+    Assignment,
+    names,
+    Person,
+    repeat,
+    Schedule,
+    Task,
+} from '~/lib/schedule';
 
 const lockedSchedule = [
-  {
-    'Living Room': ['Marko', 'Olga'],
-    'Toilets': ['Dimitra'],
-    'Bathroom': ['Gabriele'],
-    'Showers': ['Kristofers'],
-    'Hallways': ['Inês'],
-    'Kitchen': ['Marlou', 'Irene', 'Estephania'],
-    'Laundry Room': ['Alex'],
-  },
-  {
-    'Living Room': ['Mony', 'Michelle'],
-    'Toilets': ['Diego'],
-    'Bathroom': ['Inês'],
-    'Showers': ['Gabriele'],
-    'Hallways': ['Olga'],
-  },
-  {
-    'Living Room': ['Inês', 'Gabriele'],
-    'Toilets': ['Olga'],
-    'Bathroom': ['Marko'],
-    'Showers': ['Mony'],
-    'Hallways': ['Marlou'],
-  },
-  {
-    'Living Room': ['Kristofers', 'Irene'],
-    'Toilets': ['Marko'],
-    'Bathroom': ['Dimitra'],
-    'Showers': ['Alex'],
-    'Hallways': ['Gabriele'],
-  },
-  {
-    'Living Room': [],
-    'Toilets': ['Irene'],
-    'Bathroom': ['Michelle'],
-    'Showers': ['Olga'],
-    'Hallways': ['Diego'],
-    'Kitchen': ['Marko', 'Kristofers', 'Gabriele'],
-    'Laundry Room': ['Mony'],
-  },
-  {
-    'Living Room': ['Marlou', 'Diego'],
-    'Toilets': ['Estephania'],
-    'Bathroom': ['Irene'],
-    'Showers': ['Dimitra'],
-    'Hallways': ['Marko'],
-  },
+    {
+        'Living Room': ['Marko', 'Olga'],
+        Toilets: ['Dimitra'],
+        Bathroom: ['Gabriele'],
+        Showers: ['Kristofers'],
+        Hallways: ['Inês'],
+        Kitchen: ['Marlou', 'Irene', 'Estephania'],
+        'Laundry Room': ['Alex'],
+    },
+    {
+        'Living Room': ['Mony', 'Michelle'],
+        Toilets: ['Diego'],
+        Bathroom: ['Inês'],
+        Showers: ['Gabriele'],
+        Hallways: ['Olga'],
+    },
+    {
+        'Living Room': ['Inês', 'Gabriele'],
+        Toilets: ['Olga'],
+        Bathroom: ['Marko'],
+        Showers: ['Mony'],
+        Hallways: ['Marlou'],
+    },
+    {
+        'Living Room': ['Kristofers', 'Irene'],
+        Toilets: ['Marko'],
+        Bathroom: ['Dimitra'],
+        Showers: ['Alex'],
+        Hallways: ['Gabriele'],
+    },
+    {
+        'Living Room': [],
+        Toilets: ['Irene'],
+        Bathroom: ['Michelle'],
+        Showers: ['Olga'],
+        Hallways: ['Diego'],
+        Kitchen: ['Marko', 'Kristofers', 'Gabriele'],
+        'Laundry Room': ['Mony'],
+    },
+    {
+        'Living Room': ['Marlou', 'Diego'],
+        Toilets: ['Estephania'],
+        Bathroom: ['Irene'],
+        Showers: ['Dimitra'],
+        Hallways: ['Marko'],
+    },
 ] as unknown as Assignment[];
 
 const tasks = [
-  { name: 'Living Room', people: 2, kind: repeat.weekly },
-  { name: 'Toilets', people: 1, kind: repeat.weekly },
-  { name: 'Bathroom', people: 1, kind: repeat.weekly },
-  { name: 'Showers', people: 1, kind: repeat.weekly },
-  { name: 'Hallways', people: 1, kind: repeat.weeklyWithMonthly },
-  { name: 'Kitchen', people: 3, kind: repeat.monthly },
-  { name: 'Laundry Room', people: 1, kind: repeat.monthly },
+    { name: 'Living Room', people: 2, kind: repeat.weekly },
+    { name: 'Toilets', people: 1, kind: repeat.weekly },
+    { name: 'Bathroom', people: 1, kind: repeat.weekly },
+    { name: 'Showers', people: 1, kind: repeat.weekly },
+    { name: 'Hallways', people: 1, kind: repeat.weeklyWithMonthly },
+    { name: 'Kitchen', people: 3, kind: repeat.monthly },
+    { name: 'Laundry Room', people: 1, kind: repeat.monthly },
 ] as Task[];
 
 const people = [
-  'Inês',
-  'Gabriele',
-  'Olga',
-  'Marko',
-  'Mony',
-  'Marlou',
-  'Estephania',
-  'Dimitra',
-  'Irene',
-  'Kristofers',
-  'Alex',
-  'Michelle',
-  'Diego',
+    'Inês',
+    'Gabriele',
+    'Olga',
+    'Marko',
+    'Mony',
+    'Marlou',
+    'Estephania',
+    'Dimitra',
+    'Irene',
+    'Kristofers',
+    'Alex',
+    'Michelle',
+    'Diego',
 ] as Person[];
 
-function* generateSchedule(numWeeks: number, people: Person[], tasks: Task[]) {
-  let counters = initCounters(people, names(tasks));
-  let date = new Date('09-02-2024');
-  console.log({ date });
-  seed(912312423333);
-
-  for (const week of lockedSchedule) {
-    yield [date, tasks.map(({ name }) => week[name] ?? [])] as [Date, string[][]];
-    updateCounters(counters, week);
-    addWeek(date);
-    console.log({ date });
-  }
-
-  for (let i = lockedSchedule.length; i < numWeeks; i++) {
-    try {
-      if (date.toString() === (new Date('12-02-2024')).toString()) {
-        const ivo = "Ivo" as Person
-        people = people.filter(person => person != 'Marlou')
-        people.push(ivo)
-        addPerson(ivo, counters);
-      }
-
-      let pool = i > 5 && i < 10 ? people.filter(person => person != 'Alex') : people;
-      pool = i == 6 ? exclude(pool, ['Estephania'] as Person[]) : pool;
-
-      const assignment = nextWeekTasks(pool, isStartOfMonth(date) ? tasks : weekly(tasks), counters);
-      yield [new Date(date), tasks.map(({ name }) => assignment[name] ?? [])] as [Date, string[][]];
-      addWeek(date);
-    } catch (e) {
-      console.log(e);
-      return;
-    }
-  }
-}
-
-const names = (tasks: Task[]) => tasks.map(({ name }) => name);
-
-const weekly = (tasks: Task[]) => tasks.filter(({ kind }) => kind !== repeat.monthly)
-
-const exclude = (people: readonly Person[], excluded: Person[]) => people.filter(person => !excluded.includes(person))
-
 export default function Home() {
-  const [weeks, setWeeks] = createSignal<number | null>(null);
-  const [rows, setRows] = createSignal<[Date, string[][]][] | null>(null);
-  const tasksNames = names(tasks);
-  // tasksNames.push('counters');
+    const schedule = new Schedule(new Date('09-02-2024'), people, tasks);
+    schedule.lock(lockedSchedule);
+    const [weeks, setWeeks] = createSignal<number | null>(null);
+    const [rows, setRows] = createSignal<[Date, string[][]][] | null>(null);
+    const tasksNames = names(tasks);
 
-  const generate = () => {
-    const n = weeks();
-    if (n === null) return [];
-    const newRows = Array.from(generateSchedule(n, people, tasks));
-    setRows(newRows);
-  };
+    const generate = () => {
+        const n = weeks();
+        if (n === null) return [];
+        const newRows = Array.from(schedule.assignments().take(n));
+        setRows(newRows);
+    };
 
-  const clear = () => {
-    setRows(null);
-  };
+    const clear = () => {
+        setRows(null);
+    };
 
-  return (
-    <div class="ml-4">
-      <h1>Schedule</h1>
-      <Switch>
-        <Match when={rows() === null}>
-          <input
-            type="number"
-            class="m-2 p-2 border rounded w-20"
-            value={weeks() ?? 0}
-            onInput={(e) => setWeeks(Number.parseInt(e.target.value))}
-          />
-          <button class="m-2 p-2 rounded bg-blue-800 text-white" onClick={generate}>Generate</button>
-        </Match>
-        <Match when={rows() !== null}>
-          <TaskTable
-            rows={rows() as [Date, string[][]][]}
-            tasks={tasksNames}
-          />
-          <button class="m-2 p-2 rounded bg-blue-800 text-white" onClick={clear}>Clear</button>
-        </Match>
-      </Switch>
-    </div>
-  );
+    return (
+        <div class="ml-4">
+            <h1>Schedule</h1>
+            <Switch>
+                <Match when={rows() === null}>
+                    <input
+                        type="number"
+                        class="m-2 p-2 border rounded w-20"
+                        value={weeks() ?? 0}
+                        onInput={(e) =>
+                            setWeeks(Number.parseInt(e.target.value))
+                        }
+                    />
+                    <button
+                        class="m-2 p-2 rounded bg-blue-800 text-white"
+                        onClick={generate}
+                    >
+                        Generate
+                    </button>
+                </Match>
+                <Match when={rows() !== null}>
+                    <TaskTable
+                        rows={rows() as [Date, string[][]][]}
+                        tasks={tasksNames}
+                    />
+                    <button
+                        class="m-2 p-2 rounded bg-blue-800 text-white"
+                        onClick={clear}
+                    >
+                        Clear
+                    </button>
+                </Match>
+            </Switch>
+        </div>
+    );
 }
 
 function TaskTable(props: { tasks: string[]; rows: [Date, string[][]][] }) {
